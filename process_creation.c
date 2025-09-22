ff177ab801f6c5d38e55bb2d28602bbb0eb4b817
Alice Vang <alicevang18@gmail.com>
2025-09-22 21:27:19 +0000

    almost got it

--- process_creation.c
+++ process_creation.c
@@ -1,27 +1,49 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+
+void signal_handler(int sig){
+	exit(5);
+}
 
 int main(){
 	pid_t pid;
 
 	// fork a child process
 	pid = fork();
-	
-	printf("Child process PID: %d\n", pid);
+
+	// parent ignores signal
+	signal(SIGINT, SIG_IGN);
 
 	if(pid < 0){
-		fprintf(stderr, "Fork failed.\n");
+		fprintf(stderr, "Error: fork failed.\n");
 		return 1;
+
 	// child process
 	} else if(pid == 0){
-		execlp("/bin/ls","ls",NULL);
-	} else {
-		//parent process; parent will wait for child to complete
-		printf("Child process PID: %d\n", pid);
-		wait(NULL);
-		printf("Child complete\n");
+		printf("%d\n", getpid());
+
+		signal(SIGINT, signal_handler);
+		if(signal(SIGINT, signal_handler) == SIG_ERR){
+			fprintf(stderr, "Error: unable to set signal handler for SIGINT.\n");
+			return 1;
+		}
+		pause();
+		exit(5);
+	
+	// parent process; parent will wait for child to finish	
+       	} else {
+		int status;
+		waitpid(pid, &status, 0);
+		if(WIFEXITED(status)){
+			printf("childpid=%d, exitstatus=%d\n", pid, WEXITSTATUS(status));
+		} else if(WIFSIGNALED(status)){
+			fprintf(stderr,"Error: child terminated by signal %d.\n",WTERMSIG(status));
+		}
+		
 	}
 	return 0;
 }
